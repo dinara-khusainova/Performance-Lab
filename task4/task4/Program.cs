ad18d6659e0b512e5fdd7be8e5e03310fbf313b6
Dinara Khusainova <149240267+dinara-khusainova@users.noreply.github.com>
2023-12-22 11:49:01 +0300

    Add files via upload

---
+++ task4/task4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Используйте в виде аргумента файл input_file.txt");
+            return;
+        }
+
+        string inputFile = args[0];
+
+        try
+        {
+            int[] nums = File.ReadAllLines(inputFile).Select(int.Parse).ToArray();
+
+            int moves = MinMovesToEqualize(nums);
+
+            Console.WriteLine(moves);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+
+    static int MinMovesToEqualize(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            throw new ArgumentException("Массив чисел не может быть пустым.");
+        }
+
+        int moves = 0;
+        int target = nums[nums.Length / 2]; // Медиана массива как целевое значение
+
+        foreach (int num in nums)
+        {
+            moves += Math.Abs(num - target);
+        }
+
+        return moves;
+    }
+}
